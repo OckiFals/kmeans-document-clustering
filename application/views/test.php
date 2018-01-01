d3c221d021f86ee881d101bfb766354bbe4bc846
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2018-01-02 02:29:47 +0700

    initialize abstracts id

--- application/views/test.php
+++ application/views/test.php
@@ -78,16 +78,20 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT abstrak FROM tambah_dokumen";
+$sql = "SELECT id_dokumen, abstrak FROM tambah_dokumen";
+$abstrak_id = [];
 $abstrak = array();
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
+	$index = 0;
 	while ($row = $result->fetch_array()) {
-		$abstrak[] = $row[0];
+		$abstrak_id['d'.($index+1)] = $row[0];
+		$abstrak[] = $row[1];
+		$index++;
 	}
 }
 
-$conn->close();
+// $conn->close();
 
 $terms = [];
 $terms_temp = [];
