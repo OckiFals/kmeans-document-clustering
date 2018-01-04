ebec38aee607015b490c5167be623fcce3e0c728
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2018-01-04 23:16:58 +0700

    avatar image-url using absolute path

--- application/views/header.php
+++ application/views/header.php
@@ -62,13 +62,13 @@
                 <ul class="nav navbar-nav">
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                            <img src="<?php echo base_url('assets/dist/img/user2-160x160.jpg') ?>" class="user-image" alt="User Image">
                             <span class="hidden-xs">User</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- User image -->
                             <li class="user-header">
-                                <img src="assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                <img src="<?php echo base_url('assets/dist/img/user2-160x160.jpg') ?>" class="img-circle" alt="User Image">
 
                                 <p>
                                     User
