ad3e653e877bd9f21c489344989edc7ff6147919
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2018-03-03 18:12:36 +0700

    Update sidebar

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -4,68 +4,43 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
             <li class="header">MAIN NAVIGATION</li>
-
-
-            <li class="<?php if ($this->uri->segment(1) == "") {
-                echo "active";
-            } ?>">
+            <li class="<?php echo ($this->uri->segment(1) == "") ? 'active' : '' ?>">
                 <a href="<?php echo base_url('') ?>">
                     <i class="fa fa-edit"></i> <span>Data Skripsi</span>
-                    <span class="pull-right-container">
-              <!-- <i class="fa fa-angle-left pull-right"></i> -->
-            </span>
+                    <span class="pull-right-container"></span>
                 </a>
-                <!-- <ul class="treeview-menu">
-
-                </ul> -->
             </li>
 
             </li>
-            <li class="<?php if ($this->uri->segment(1) == "treeview-menu") {
-                echo "treeview active";
-            } ?>">
+            <li class="<?php echo ($this->uri->segment(1) == "stopwords") ? 'treeview active' : '' ?>">
                 <a href="#">
                     <i class="fa fa-table"></i> <span>Text Preprocessing</span>
                     <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+              			<i class="fa fa-angle-left pull-right"></i>
+           			</span>
                 </a>
                 <ul class="treeview-menu">
-
-                    <li class="<?php if ($this->uri->segment(1) == 'stopwords') {
-                        echo "active";
-                    } ?>">
-                        <a href="stopwords"><i class="fa fa-circle-o"></i> StopWords</a></li>
-
-                    <li class="<?php if ($this->uri->segment(1) == '') {
-                        echo "active";
-                    } ?>">
-                        <a href="manual"><i class="fa fa-circle-o"></i> Manualisasi</a></li>
-
-
-                </ul>
+                    <li class="<?php echo ($this->uri->segment(1) == 'stopwords') ? 'active' : '' ?>">
+                        <a href="stopwords"><i class="fa fa-circle-o"></i> StopWords</a>
+					</li>
+               	</ul>
             </li>
 
-            <li class="<?php if ($this->uri->segment(1) == "treeview-menu") {
-                echo "treeview active";
-            } ?>">
+            <li class="<?php echo ($this->uri->segment(1) == "klustering") ? 'treeview active' : '' ?>">
                 <a href="#">
                     <i class="fa fa-table"></i> <span>Clustering</span>
                     <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+              			<i class="fa fa-angle-left pull-right"></i>
+            		</span>
                 </a>
                 <ul class="treeview-menu">
-
-                    <li class="<?php if ($this->uri->segment(1) == 'klustering') {
-                        echo "active";
-                    } ?>">
-                        <a href="<?php echo base_url('klustering') ?>"><i class="fa fa-circle-o"></i> K-Means</a></li>
-
+                    <li class="<?php echo ($this->uri->segment(1) == 'klustering') ? 'active' : '' ?>">
+                        <a href="<?php echo base_url('klustering') ?>">
+							<i class="fa fa-circle-o"></i> K-Means
+						</a>
+					</li>
                 </ul>
             </li>
-
-
         </ul>
     </section>
     <!-- /.sidebar -->
