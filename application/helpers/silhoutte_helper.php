9be035d8703c62b46fe951538c5175937478dbef
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2018-04-01 17:58:45 +0700

    Sum outer cluster distance and average
    Sum distance and average document distance with documents in outer cluster

--- application/helpers/silhoutte_helper.php
+++ application/helpers/silhoutte_helper.php
@@ -32,8 +32,12 @@ class Silhoutte_helper extends CI_Model {
         $results = [];
         // inner cluster
         $distance_a = [];
+        $average_a = [];
         // outer cluster
         $distance_b = [];
+        $average_b = [];
+
+        // inner cluster distance
         foreach ($this->cluster_kmean as $cluster => $docs) {
             $docs_keys = array_keys($docs);
             foreach ($docs as $doc) {
@@ -42,17 +46,34 @@ class Silhoutte_helper extends CI_Model {
                     if ($doc === $docs_keys[$i]) {
                         continue;
                     }
-                    $distance_a[$cluster]["(${doc},${docs_keys[$i]})"] = 1 - $this->innerClusterDistance($doc, $docs_keys[$i]);
+                    $distance_a[$cluster]["(${doc},${docs_keys[$i]})"] = 1 - $this->clusterDistance($doc, $docs_keys[$i]);
                 }
             }
         }
 
-        // average distance of inner cluster
+        // average document distance with documents in outer cluster
         foreach ($this->cluster_kmean as $cluster_index => $cluster) {
             foreach ($cluster as $document) {
-                $results[] = $this->averageDistance($distance_a[$cluster_index], $document);
+                $average_a["b(${document})"] = $this->averageInnerDistance($distance_a[$cluster_index], $document);
             }
         }
+
+        // outer cluster distance
+        $cluster_kmean_index = array_keys($this->cluster_kmean);
+        foreach ($this->cluster_kmean as $cluster => $docs) {
+            foreach ($docs as $doc) {
+                foreach ($cluster_kmean_index as $cl) {
+                    if ($cluster !== $cl) {
+                        foreach ($this->cluster_kmean[$cl] as $doc_) {
+                            $distance_b["d(${doc},${doc_})"] = 1 - $this->clusterDistance($doc, $doc_);
+                        }
+                    }
+                }
+            }
+        }
+
+        // average document distance with documents in outer cluster
+        $this->averageOuterDistance($distance_b, $average_b);
 
         return $results;
     }
@@ -62,7 +83,7 @@ class Silhoutte_helper extends CI_Model {
      * @param string $m document n+1
      * @return float
      */
-    public function innerClusterDistance($n, $m) {
+    public function clusterDistance($n, $m) {
         $distance = 0;
 
         foreach ($this->tfidf as $term => $doc) {
@@ -77,16 +98,70 @@ class Silhoutte_helper extends CI_Model {
      * @param $document string document name
      * @return int average distance
      */
-    public function averageDistance($distance, $document) {
+    public function averageInnerDistance($distance, $document) {
         $average = [];
 
         foreach ($distance as $index => $value) {
-            if (preg_match("(${document},d[1-9])", $index)) {
+            if (preg_match("(${document},d[0-9]+)", $index)) {
                 $average[] = $value;
             }
         }
 
         return array_sum($average) / count($average);
+    }
+
+    /**
+     * @param $distance array cluster distance
+     * @param $average_b
+     */
+    public function averageOuterDistance($distance, &$average_b) {
+        /*
+         * Flip cluster kmean array, from:
+         *
+         * array(
+         *  'c1' => array('d1', 'd2', ..),
+         *  'c2' => array('d8', ..)
+         * )
+         *
+         * To:
+         *
+         * array(
+         *  'd1' => 'c1',
+         *  'd2 => 'c1',
+         *  ...
+         *  'd8' => 'c2'
+         * )
+         */
+        $cluster_kmean_flip = [];
+        foreach ($this->cluster_kmean as $cluster => $docs) {
+            foreach ($docs as $doc) {
+                $cluster_kmean_flip[$doc] = $cluster;
+            }
+        }
+
+        // distance before average
+        $distance_b_raw = [];
+        foreach ($distance as $key => $value) {
+            preg_match("(d[0-9]+,d[0-9]+)", $key, $matches);
+            $doc = explode(',', $matches[0]);
+            /*
+             * [
+             *  'd1(c2)' => ‌[
+             *    'd1,d2' => 0.853998629248431,
+             *    'd1,d3' => 0.8898405206140072
+             *  ],
+             *  'd1(c3)' => [
+             *    'd1,d5' => 0.7873851886817778
+             *  ]
+             *  .....
+             * ]
+             */
+            $distance_b_raw[$doc[0] . '(' . $cluster_kmean_flip[$doc[1]] . ')'][$matches[0]] = $value;
+        }
+
+        foreach ($distance_b_raw as $key => $value) {
+            $average_b[$key] = array_sum($value);
+        }
     }
 
     /**
